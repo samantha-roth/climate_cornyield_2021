071cc5f180dfe474b1aaba0e3ca3f7c6cef9e3bc
samantha-roth <41164073+samantha-roth@users.noreply.github.com>
2021-06-07 23:36:22 -0400

    Update reorder_by_year.R

--- reorder_by_year.R
+++ reorder_by_year.R
@@ -1,9 +1,48 @@
-#reorder step3norm by year instead of location
+#reorder pwl_norm by year instead of location
+rm(list = ls())
 
 library(tidyverse)
 
-load("C:/Users/saman/Dropbox/Climate_CornYield-me/SourceData/METDATA/timeloc_df")
-load("C:/Users/saman/Dropbox/Climate_CornYield-me/SourceData/METDATA/step3norm")
+load("/storage/work/svr5482/Climate_CornYield-me/SourceData/METDATA/timeloc_df")
+load("/storage/work/svr5482/Climate_CornYield-me/SourceData/METDATA/pwl_norm")
+
+#format the data so it's in order of year instead of location
+
+year<- as.character(timeloc_df$year)
+year<- as.numeric(year)
+
+year<- year[order(year)]
+
+pwln.ord<- matrix(NA, ncol= ncol(pwl_norm), nrow= nrow(pwl_norm))
+pwln.ord<- as.data.frame(pwln.ord)
+colnames(pwln.ord)<- colnames(pwl_norm)
+
+for(i in 1:32){
+  inds<- which(timeloc_df$year==1980+i)
+  pwln.ord[which(year==1980+i),]<- pwl_norm[inds,]
+}
+
+save(pwln.ord, file="/storage/work/svr5482/Climate_CornYield-me/SourceData/METDATA/pwl_norm_yrorder")
+
+#reorder timeloc_df so it's ordered by year
+
+timeloc.ord<- matrix(NA, ncol= ncol(timeloc_df), nrow= nrow(timeloc_df))
+timeloc.ord<- as.data.frame(timeloc.ord)
+colnames(timeloc.ord)<- colnames(timeloc_df)
+
+timeloc_df$fips<- as.character(timeloc_df$fips)
+timeloc_df$StateANSI<- as.character(timeloc_df$StateANSI)
+timeloc_df$year<- as.character(timeloc_df$year)
+
+for(i in 1:32){
+  inds<- which(timeloc_df$year==1980+i)
+  timeloc.ord[which(year==1980+i),]<- timeloc_df[inds,]
+}
+
+save(timeloc.ord, file="/storage/work/svr5482/Climate_CornYield-me/SourceData/METDATA/timeloc_yrorder")
+
+#reorder step3norm by year instead of location
+load("/storage/work/svr5482/Climate_CornYield-me/SourceData/METDATA/step3norm")
 
 #format the data so it's in order of year instead of location
 
@@ -21,19 +60,4 @@ for(i in 1:32){
   step3n.ord[which(year==1980+i),]<- step3norm[inds,]
 }
 
-save(step3n.ord, file="C:/Users/saman/Dropbox/Climate_CornYield-me/SourceData/METDATA/step3norm_yrorder")
-
-timeloc.ord<- matrix(NA, ncol= ncol(timeloc_df), nrow= nrow(timeloc_df))
-timeloc.ord<- as.data.frame(timeloc.ord)
-colnames(timeloc.ord)<- colnames(timeloc_df)
-
-timeloc_df$fips<- as.character(timeloc_df$fips)
-timeloc_df$StateANSI<- as.character(timeloc_df$StateANSI)
-timeloc_df$year<- as.character(timeloc_df$year)
-
-for(i in 1:32){
-  inds<- which(timeloc_df$year==1980+i)
-  timeloc.ord[which(year==1980+i),]<- timeloc_df[inds,]
-}
-
-save(timeloc.ord, file="C:/Users/saman/Dropbox/Climate_CornYield-me/SourceData/METDATA/timeloc_yrorder")
+save(step3n.ord, file="/storage/work/svr5482/Climate_CornYield-me/SourceData/METDATA/step3norm_yrorder")
